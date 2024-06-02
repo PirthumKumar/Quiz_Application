a8639751e349b8e03b739d41840d95df655b7c3b
PirthumKumar <pirthummankani03@gmail.com>
2024-06-02 17:56:26 +0500

    Add files via upload

---
+++ Quiz_Application/src/main/java/worldcom/mycompany/quiz_application/Quiz_Application.java
@@ -0,0 +1,16 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ */
+
+package worldcom.mycompany.quiz_application;
+
+/**
+ *
+ * @author pirthum
+ */
+public class Quiz_Application {
+
+    public static void main(String[] args) {
+        System.out.println("Hello World!");
+    }
+}
